8084973c4949b79a14a3615e2526d6d8191a3c9e
MuhammadMahi585 <maheemahesar39@gmail.com>
2024-05-13 13:14:15 -0700

    git add .

---
+++ app/src/main/java/com/mahi/birthdaycardapp/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.mahi.birthdaycardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mahi.birthdaycardapp.ui.theme.BirthdayCardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(message = "Happy Birthday Ali",
+                        from = " from Mahi" ,
+                    modifier = Modifier.padding(8.dp))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(message:String,from:String,modifier: Modifier=Modifier){
+        Column(
+            verticalArrangement= Arrangement.Center,
+            modifier=modifier.padding(8.dp)) {
+    Text(
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
+        )
+                Text(
+                    text = from,
+                    fontSize = 36.sp,
+                    modifier=modifier.padding(16.dp).align(alignment = Alignment.End)
+
+                )
+            }
+    }
+@Composable
+fun GreetingImage(message: String,from: String){
+
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BirthdayCardAppTheme {
+        GreetingText(message = "Happy Birthday Ali!",from="from Mahi")
+    }
+}
